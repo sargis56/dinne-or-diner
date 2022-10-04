eaa2c83734e35521a83dc39bafdbc3790f3c5793
SSwintak <samswintak@gmail.com>
2022-10-03 22:51:20 -0400

    Adding classes(functionality WIP)

---
+++ DinnerOrDiner_UE/Source/DinnerOrDiner_UE/Bear.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bear.h"
+
+// Sets default values
+ABear::ABear()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABear::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABear::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABear::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ DinnerOrDiner_UE/Source/DinnerOrDiner_UE/Bear_AIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Bear_AIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DINNERORDINER_UE_API ABear_AIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
